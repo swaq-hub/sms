06d637f1609ba2ae2c0217e0c8d09ea2a6bee47f
JoeMayami <36084817+JoeMayami@users.noreply.github.com>
2019-04-14 18:54:59 +0100

    Fixing bugs

--- sms/settings.py
+++ sms/settings.py
@@ -81,9 +81,15 @@ WSGI_APPLICATION = 'sms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 prod_db  =  dj_database_url.config(conn_max_age=500)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 DATABASES['default'].update(prod_db)
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
