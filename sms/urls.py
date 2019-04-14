3318872eb17247826bb567d7cbd23b09070cf1ba
JoeMayami <36084817+JoeMayami@users.noreply.github.com>
2019-04-14 18:24:08 +0100

    Coming Soon
    Added a coming soon page

--- sms/urls.py
+++ sms/urls.py
@@ -1,7 +1,7 @@
 """sms URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.urls import path
+from django.contrib import admin
+from . import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home),
+    
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ sms/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.contrib import auth
+from django.template import Template, Context
+
+
+
+def home(request):
+    #commingsoon
+    
+
+    return render(request,"index.html")
+
