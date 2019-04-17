269ed5d5ed4fcd93bcbd0443648f8db398e9a68a
JoeMayami <36084817+JoeMayami@users.noreply.github.com>
2019-04-17 17:16:24 +0100

    upadate

--- schoolerp/settings.py
+++ schoolerp/settings.py
@@ -121,18 +121,22 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-
 STATIC_URL = '/static/'
+
+#------------------------------------
+#ADDED django documentation requires this for loading static files.
+#------------------------------------
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
+    # '/var/www/static/',
 ]
+#------------------------------------
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media"),
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn") #ADDED this for setting directory path for static root in static_cdn folder.
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn") #ADDED this for setting directory path for media root in media_cdn folder.
 
 
 
